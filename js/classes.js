081e5111c882c8cf369096c5b45134bca671b1f3
Mohamad Abdulwahab <mo.abwhab@gmail.com>
2026-02-21 15:56:44 +0200

    register & login feature done

--- js/classes.js
+++ js/classes.js
@@ -1,10 +1,11 @@
 class User
 {
-    constructor(name = '', email = '', password = '')
+    constructor(name = '', email = '', password = '', guffa = null)
     {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.guffa = guffa;
     }
 }
 
@@ -13,6 +14,28 @@ class User
 //     constructor(name = '', price = '', category = '')
 //     {
 
+//     }
+// }
+
+// class Guffa
+// {
+//     constructor(products = null)
+//     {
+//         this.count = 0;
+//         this.products = products;
+//     }
+
+//     AddToGuffa(product) {
+//         this.products.push(product);
+//         this.count++;
+//     }
+
+//     RemoveFromGuffa(product)
+//     {
+//         let arr = [];
+//         arr.findIndex(p => p["Name"] == )
+//         this.products.indexOf()
+//        this.products.splice() 
 //     }
 // }
 
--- js/script.js
+++ js/script.js
@@ -1,60 +1,179 @@
 import { User } from "./classes.js";
 
-const loginForm = document.querySelector("login-form");
-const registerForm = document.querySelector("register-form");
-
-function getFormValues(formObj)
+(function loadData() //used when loading body
 {
-    const data = new FormData(formObj);
-    const formValuesObj = Object.fromEntries(data.entries());
-    return formValuesObj;
+    //TODO: fetch products from database
+    if(localStorage.getItem("emails") == null)
+    {
+        localStorage.setItem("emails", JSON.stringify([]));
+    }
+    if(localStorage.getItem("users") == null)
+    {
+        localStorage.setItem("users", JSON.stringify([]));
+    }
+}
+)();
+
+function validateFieldByPattern(formField, pattern, errorMessage)
+{
+    let fieldValid = true;
+    let elemSpan = formField.nextElementSibling;
+    if(!formField.value.match(pattern))
+    {
+        elemSpan.setAttribute("class", "validate-invalid");
+        elemSpan.textContent = errorMessage;
+        fieldValid = false;
+    }
+    else
+    {
+        elemSpan.setAttribute("class", "validate-valid");
+        elemSpan.textContent = "";
+        fieldValid = true;
+    }
+    return fieldValid;
 }
 
-function frontValidate(formObj)
+function registerValidate()
 {
     let isValid = true;
 
-    const name = formObj.elements['name'];
-    const email = formObj.elements['email'];
-    const password = formObj.elements['password'];
-    const passwordConfirm = formObj.elements['password-confirm'];
+    const name = document.getElementById("register-name");
+    const namePattern = /[a-zA-Z\s]/;
+    const nameErrorMsg = "Name must be alphabetical characters only";
+    let nameValid = validateFieldByPattern(name, namePattern, nameErrorMsg);
+
+    const email = document.getElementById("register-email");
+    const emailPattern = /\w{3,}@(yahoo|gmail|outlook).com/;
+    const emailErrorMsg = "Email must be yahoo, gmail, or outlook";
+    let emailValid = validateFieldByPattern(email, emailPattern, emailErrorMsg);
+
+    const password = document.getElementById("register-password");
+    const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]+$/;
+    const passwordErrorMsg = "Password must contain capital, small letters and numbers";
+    let passwordValid = validateFieldByPattern(password, passwordPattern, passwordErrorMsg);
+
+    const passwordConfirm = document.getElementById("register-password-confirm");
+    const passwordConfirmErrorMsg = "Passwords don't match";
+    let passwordConfirmValid = (password.value == passwordConfirm.value);
+    let confirmSpan = passwordConfirm.nextElementSibling;
+
+    if(!passwordConfirmValid)
+    {
+        confirmSpan.setAttribute("class", "validate-invalid");
+        confirmSpan.textContent = passwordConfirmErrorMsg;
+    }
+    else
+    {
+        confirmSpan.setAttribute("class", "validate-valid");
+        confirmSpan.textContent = "";
+    }
+
+    if(!(nameValid && emailValid && passwordValid && passwordConfirmValid))
+    {
+        isValid = false;
+    }
 
     return isValid;
 }
 
-function backValidate(formObj)
+function checkUserExistence(email)
 {
-    let isValid = true;
-
-    //Get user from local storage where email = email
-        //if exists
-            //check password = password
-                //if correct 
-                    //forward to nextpage
-                //else
-                    //show 'no users match the input data'
-
-    return isValid;
+    let userExists = false;
+    let emails = JSON.parse(localStorage.getItem("emails"));
+    for(let i of emails)
+    {
+        if(i == email)
+        {
+            userExists = true;
+        }
+    }
+    return userExists;
 }
 
-function login(event)
-{
-    event.preventDefault();
-    //if backValidate()
-        //forward to homepage
-    //else
-        //show error message
-}
-
-function register(event)
+function registerUser(event)
 {
     event.preventDefault();
-    //if (validate(event.target))
-        //let obj = getFormData(target)
-            //if !checkExistnce()
-                //store obj in local storage under Users
-                //store obj email under Emails (for existence check)
+    if (!registerValidate()) return;
+
+    const name = document.getElementById("register-name").value;
+    const email = document.getElementById("register-email").value;
+    const password = document.getElementById("register-password").value;
+
+    let user = new User(name, email, password);
+
+    if (!checkUserExistence(user.email))
+    {
+        let users = JSON.parse(localStorage.getItem("users"));
+        let emails = JSON.parse(localStorage.getItem("emails"));
+
+        users.push(user);
+        emails.push(user.email);
+
+        localStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("emails", JSON.stringify(emails));
+        
+        showAlert("User registered successfully", "alertBox", redirectToIndex);
+    }
+    else
+    {
+        showAlert("User Already Exists!", "alertBox", ()=>{});
+    }
+    
 }
 
-loginForm.addEventListener('submit', getFormValues);
-registerForm.addEventListener('submit', getFormValues);
+window.registerUser = registerUser;
+window.loginUser = loginUser;
+
+function loginUser(event)
+{
+    event.preventDefault();
+
+    let email = document.getElementById("login-email").value;
+    let password = document.getElementById("login-password").value;
+
+    let loginValid = false;
+
+    let users = JSON.parse(localStorage.getItem("users"));
+    for (const element of users) 
+    {
+        if(element["email"] == email && element["password"] == password)
+        {
+            loginValid = true;
+        }
+    }
+
+    if(loginValid)
+    {
+        showAlert("Login success", "alertBox", () => {})
+    }
+    else
+    {
+        showAlert("Incorrect email or password", "alertBox", () => {})
+    }
+}
+
+function showAlert(message, alertBoxId, callback) {
+  let alertBox = document.getElementById(alertBoxId);
+  let alertMsg = document.createElement("div");
+  let closeBtn = document.createElement("button");
+  
+  alertBox.replaceChildren();
+  alertMsg.textContent = message;
+  closeBtn.textContent = "OK";
+  closeBtn.onclick = () => closeAlert("alertBox", callback);
+
+  alertBox.appendChild(alertMsg);
+  alertBox.appendChild(closeBtn);
+  alertBox.style.display = "block";
+
+}
+
+function redirectToIndex()
+{
+    location.href = "./index.html";
+}
+
+function closeAlert(alertBoxId, callback) { //will be called by button OK
+  document.getElementById(alertBoxId).style.display = "none";
+  callback();
+}
